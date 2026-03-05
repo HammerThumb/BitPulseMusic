051af331ea5daf260f1f343dae38159979b5f972
HammerThumb <bryanpaden@gmail.com>
2026-03-04 22:06:38 -0500

    add game page

---
+++ app/game/page.tsx
@@ -0,0 +1,11 @@
+export default function GamePage() {
+  return (
+    <main style={{ width: "100vw", height: "100vh", margin: 0 }}>
+      <iframe
+        src="https://defense-of-sonaris.vercel.app"
+        style={{ width: "100%", height: "100%", border: "none" }}
+        allow="fullscreen"
+      />
+    </main>
+  );
+}
